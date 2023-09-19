cf044e0a56d8915f8d4172966f590ae45df918ae
sahosridoy <sahosmia.webdev@gmail.com>
2023-09-19 14:26:05 +0600

    solve

--- src/components/BookApp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const BookApp = () => {
-  return (
-    <div>BookApp</div>
-  )
-}
-
-export default BookApp
--- src/data/components.tsx
+++ src/data/components.tsx
@@ -4,7 +4,6 @@ export { default as Hero } from "../components/Hero";
 export { default as Map} from "../components/Map";
 export { default as Contact } from "../components/Contact";
 export { default as Service } from "../components/Service";
-export { default as BookApp } from "../components/BookApp";
 export { default as Branding } from "../components/Branding";
 export { default as OurWork } from "../components/OurWork";
 
